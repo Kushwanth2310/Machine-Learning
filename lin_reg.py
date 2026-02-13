8a49238757d1f9ec12157f6f5536a5acdf6aaedc
Kushwanth2310 <kushwanthreddy.s1023@gmail.com>
2026-02-13 14:53:59 +0530

    Plot in linear regression

--- lin_reg.py
+++ lin_reg.py
@@ -1,4 +1,7 @@
 # x -> CGPA, y -> CTC, a -> xmean, c-> ymean, m->slope, b -> intercept 
+
+import matplotlib.pyplot as plt
+
 x = [ 6 , 7.34, 7.5, 6.5, 9]
 y = [20, 30, 30, 15, 50]
 
@@ -14,6 +17,27 @@ r = sum((x[i] - a)**2 for i in range(n))
 m = q/r
 
 b = c - m*a
+
+plt.scatter(x, y, color='blue', label='CGPA vs CTC')
+plt.xlabel('CGPA')
+plt.ylabel('CTC')
+plt.title('CGPA vs CTC')
+plt.show()
+
+plt.subplot(2,1,2)
+plt.scatter(x, y)
+
+x_line = sorted(x)
+y_line = [m*i + b for i in x_line]
+
+plt.plot(x_line, y_line)
+plt.xlabel("CGPA")
+plt.ylabel("CTC")
+plt.title("Best Fit Line")
+
+plt.tight_layout()
+plt.show()
+
 
 print("Slope(m): ", m)
 print("Intercept(n)", b)
